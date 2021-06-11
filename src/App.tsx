8b7419e615d64491c8db3ecf8927593888c911e2
Thanh Nguyen <thankz173@gmail.com>
2021-06-11 18:59:08 +0700

    fix: path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,10 @@
 import React from 'react'
-import { ReactComponent as TypescriptLogo } from '@/assets/images/typescript.svg'
-import { ReactComponent as TailwindLogo } from '@/assets/images/tailwindcss.svg'
-import { ReactComponent as ReactLogo } from '@/assets/images/react.svg'
+import About from '@/components/About'
 
 function App(): React.ReactElement {
   return (
-    <div className="min-h-screen w-full bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div className="w-2/3 sm:w-full mx-auto flex justify-between">
-              <ReactLogo className="w-10 h-auto sm:w-20" />
-              <TypescriptLogo className="w-10 h-auto sm:w-20" />
-              <TailwindLogo className="w-10 h-auto sm:w-20" />
-            </div>
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <p>
-                  A boilerplate for React application with TypeScript and
-                  Tailwind CSS, including things like:
-                </p>
-                <ul className="list-disc space-y-2">
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">Full Tailwind&apos;s color palette</p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">ESLint and Prettier configuration</p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">
-                      Craco configuration with webpack alias
-                    </p>
-                  </li>
-                </ul>
-                <div>
-                  <p>Perfect for creating a React app with TailwindCSS</p>
-                  <p className="text-xs text-gray-400">
-                    or just something I personally like
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="h-full w-full">
+      <About />
     </div>
   )
 }
